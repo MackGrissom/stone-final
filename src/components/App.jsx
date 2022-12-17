0ec6caad418011857cf3643827dfe72a14250164
Mack Grissom <mcintyregrissom@gmail.com>
2022-12-18 06:49:55 +0700

    removed duplicate routers

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -111,12 +111,7 @@ function App() {
           />
   
           
-  <Routes>
-          <Route path='/' element={< Home />} />
-          <Route path='/about' element={< About />} />
-          <Route path='/contact' element={< Contact />} />
-          <Route path='/portfolio' element={<Portfolio />}/>
-  </Routes>
+
           
           <Content
             style={{
