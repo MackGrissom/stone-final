d9cca48f44d013df2b2657c6f47a658062841017
Mack Grissom <mcintyregrissom@gmail.com>
2023-01-16 12:15:40 -0500

    removed unused file added updated prettier formatting

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,40 @@
-import React from 'react';
+import React from 'react'
 import {
   DesktopOutlined,
   FileOutlined,
   PieChartOutlined,
   TeamOutlined,
-  UserOutlined,
-} from '@ant-design/icons';
-import { CoffeeOutlined, DollarCircleOutlined, FormatPainterOutlined, HomeOutlined, ToolOutlined } from '@ant-design/icons';
-import Icon from '@ant-design/icons/lib/components/Icon';
-const { Header, Footer, Sider, Content } = Layout;
+  UserOutlined
+} from '@ant-design/icons'
+import {
+  CoffeeOutlined,
+  DollarCircleOutlined,
+  FormatPainterOutlined,
+  HomeOutlined,
+  ToolOutlined
+} from '@ant-design/icons'
+import Icon from '@ant-design/icons/lib/components/Icon'
+const { Header, Footer, Sider, Content } = Layout
 import { MailOutlined, FileTextOutlined } from '@ant-design/icons'
 import '../styles/App.css'
-import { GiFallingBoulder, GiKitchenTap } from "react-icons/gi";
-import {FaShower} from 'react-icons/fa';
-import { Link, Route, Routes } from 'react-router-dom';
-import Home from './Home';
-import Portfolio from './Portfolio';
-import About from './About';
-import Contact from './Contact';
+import { GiFallingBoulder, GiKitchenTap } from 'react-icons/gi'
+import { FaShower } from 'react-icons/fa'
+import { Link, Route, Routes } from 'react-router-dom'
+import Home from './Home'
+import Portfolio from './Portfolio'
+import About from './About'
+import Contact from './Contact'
 import Footer1 from '/home/mackgrissom/code/MackGrissom/ant-stoneworks/precisionstone/src/assets/footer/1.png'
 import Footer2 from '/home/mackgrissom/code/MackGrissom/ant-stoneworks/precisionstone/src/assets/footer/2.png'
 import Footer3 from '/home/mackgrissom/code/MackGrissom/ant-stoneworks/precisionstone/src/assets/footer/3.png'
-import  { useState, useEffect } from "react";
-import Loader from "./Loader.jsx";
-import Title from 'antd/es/skeleton/Title';
-import FloatMenu from './FloatMenu';
-import Logo from './Logo';
-import { UploadOutlined, VideoCameraOutlined } from '@ant-design/icons';
-import { theme } from 'antd';
-import { Breadcrumb, Layout, Menu } from 'antd';
-
+import { useState, useEffect } from 'react'
+import Loader from './Loader.jsx'
+import Title from 'antd/es/skeleton/Title'
+import FloatMenu from './FloatMenu'
+import Logo from './Logo'
+import { UploadOutlined, VideoCameraOutlined } from '@ant-design/icons'
+import { theme } from 'antd'
+import { Breadcrumb, Layout, Menu } from 'antd'
 
 // function getItem(label, key, icon, children) {
 //   return {
@@ -52,93 +57,108 @@ import { Breadcrumb, Layout, Menu } from 'antd';
 //   getItem('Files', '9', <FileOutlined />),
 // ];
 const Navbar = () => {
-  const [collapsed, setCollapsed] = useState(false);
+  const [collapsed, setCollapsed] = useState(false)
   const {
-    token: { colorBgContainer },
-  } = theme.useToken();
+    token: { colorBgContainer }
+  } = theme.useToken()
   return (
     <Layout
       style={{
-        minHeight: '100vh',
+        minHeight: '100vh'
       }}
     >
-      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+      <Sider
+        collapsible
+        collapsed={collapsed}
+        onCollapse={(value) => setCollapsed(value)}
+      >
         <div
           style={{
             height: 32,
             margin: 16,
-            background: 'black',
+            background: 'black'
           }}
         />
-      
+
         {/* <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} /> */}
-        <Icon component={Logo} style={{padding:10}} />
-            <Menu 
-            defaultSelectedKeys={'Home'}
-            mode={'inline'} style={{ background:'#161719', color:'white' }}>
+        <Icon component={Logo} style={{ padding: 10 }} />
+        <Menu
+          defaultSelectedKeys={'Home'}
+          mode={'inline'}
+          style={{ background: '#161719', color: 'white' }}
+        >
+          <Menu.Item
+            key="Home"
+            style={{ height: 50 }}
+            className="ant-menu-item"
+          >
+            <Link to="/" />
+            <HomeOutlined /> &nbsp; Home
+          </Menu.Item>
 
-              <Menu.Item key='Home' style={{height:50}} className='ant-menu-item'>
-                <Link to='/'/>
-              <HomeOutlined /> &nbsp;
-                Home
-                
-              </Menu.Item>
+          <Menu.Item
+            key="Instant Quote"
+            style={{ height: 50 }}
+            className="ant-menu-item"
+          >
+            <a href="https://precision_stoneworks.quotekitchenandbath.com/kitchen#/materials/countertops/countertop">
+              <DollarCircleOutlined /> &nbsp; Instant Quote
+            </a>
+          </Menu.Item>
 
+          <Menu.Item
+            key="Design Tool"
+            style={{ height: 50 }}
+            className="ant-menu-item"
+          >
+            <a href="https://precision_stoneworks.quotekitchenandbath.com/kitchen#/visualizer">
+              <ToolOutlined /> &nbsp; Design Tool
+            </a>
+          </Menu.Item>
 
-              <Menu.Item key='Instant Quote' style={{height:50}} className='ant-menu-item'>
-                <a href='https://precision_stoneworks.quotekitchenandbath.com/kitchen#/materials/countertops/countertop'>
-              <DollarCircleOutlined /> &nbsp;
-                Instant Quote
-                </a>
-
-              </Menu.Item>
-
-
-              <Menu.Item key='Design Tool' style={{height:50}} className='ant-menu-item'>
-                <a href='https://precision_stoneworks.quotekitchenandbath.com/kitchen#/visualizer'>
-              <ToolOutlined /> &nbsp;
-                Design Tool
-                </a>
-              </Menu.Item>
-
-              <Menu.Item key='Portfolio' style={{height:50}} className='ant-menu-item'>
-                <a href='/portfolio'>
-                <FormatPainterOutlined /> &nbsp;
-                Our Designs
-                </a>
-              </Menu.Item>
-              <Menu.Item key='Contact' style={{height:50}} className='ant-menu-item'>
-                <a href='/contact'>
-                <FormatPainterOutlined /> &nbsp;
-                Contact Us
-                </a>
-              </Menu.Item>
-</Menu>
+          <Menu.Item
+            key="Portfolio"
+            style={{ height: 50 }}
+            className="ant-menu-item"
+          >
+            <a href="/portfolio">
+              <FormatPainterOutlined /> &nbsp; Our Designs
+            </a>
+          </Menu.Item>
+          <Menu.Item
+            key="Contact"
+            style={{ height: 50 }}
+            className="ant-menu-item"
+          >
+            <a href="/contact">
+              <FormatPainterOutlined /> &nbsp; Contact Us
+            </a>
+          </Menu.Item>
+        </Menu>
       </Sider>
       <Layout className="site-layout">
         <Header
           style={{
             padding: 0,
-            background: 'white',
+            background: 'white'
           }}
         />
 
-        
-<Routes>
-        <Route path='/' element={< Home />} />
-        <Route path='/about' element={< About />} />
-        <Route path='/contact' element={< Contact />} />
-        <Route path='/portfolio' element={<Portfolio />}/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/portfolio" element={<Portfolio />} />
         </Routes>
-        
+
         <Content
           style={{
-            margin: '0 16px',
+            margin: '0 16px'
           }}
         >
           <Breadcrumb
             style={{
-              margin: '16px 0',
+              margin: '16px 0'
             }}
           >
             <Breadcrumb.Item>Kitchen Designs</Breadcrumb.Item>
@@ -149,28 +169,36 @@ const Navbar = () => {
             style={{
               padding: 24,
               minHeight: 360,
-              background: colorBgContainer,
+              background: colorBgContainer
             }}
           >
             Bill is a cat.
           </div>
         </Content>
         <Footer
-        style={{
-          textAlign: 'center',
-        }} className='footer-color'
-      >
-    <p className='text-center text-white text-[10px]'> 1090 Parkway Industrial Park Dr, Buford, GA 30518 <br/> <br/>
-(770) 271-6907</p>
-        <br/>
-        <span>
-        Home | Legal | Contact
-        <br/>
-        Precision Stone ©2023 Built by <a href='www.mackgrissom.io'> Mack Grissom <CoffeeOutlined size={5}/></a> 
-        </span>
-      </Footer>
+          style={{
+            textAlign: 'center'
+          }}
+          className="footer-color"
+        >
+          <p className="text-center text-white text-[10px]">
+            {' '}
+            1090 Parkway Industrial Park Dr, Buford, GA 30518 <br /> <br />
+            (770) 271-6907
+          </p>
+          <br />
+          <span>
+            Home | Legal | Contact
+            <br />
+            Precision Stone ©2023 Built by{' '}
+            <a href="www.mackgrissom.io">
+              {' '}
+              Mack Grissom <CoffeeOutlined size={5} />
+            </a>
+          </span>
+        </Footer>
       </Layout>
     </Layout>
-  );
-};
-export default Navbar;
+  )
+}
+export default Navbar
--- src/components/photos.jsx
+++ src/components/photos.jsx
@@ -11,196 +11,194 @@ import Kitchen10 from '/home/mackgrissom/code/MackGrissom/final-precision/stone-
 import Kitchen11 from '/home/mackgrissom/code/MackGrissom/final-precision/stone-final/src/assets/kitchen/kitchen11.webp'
 import Kitchen12 from '/home/mackgrissom/code/MackGrissom/final-precision/stone-final/src/assets/kitchen/kitchen12.webp'
 
-
 export const photos = [
-    {
-      src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
-      width: 4,
-      height: 3
-    },
-    {
-      src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
-      width: 1,
-      height: 1
-    },
-    {
-      src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
-      width: 3,
-      height: 4
-    },
-    {
-      src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
-      width: 3,
-      height: 4
-    },
-    {
-      src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
-      width: 3,
-      height: 4
-    },
-    {
-      src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
-      width: 4,
-      height: 3
-    },
-    {
-      src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
-      width: 3,
-      height: 4
-    },
-    {
-      src: "https://source.unsplash.com/PpOHJezOalU/800x599",
-      width: 4,
-      height: 3
-    },
-    {
-      src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
-      width: 4,
-      height: 3
-    }
-  ];
+  {
+    src: 'https://source.unsplash.com/2ShvY8Lf6l0/800x599',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://source.unsplash.com/Dm-qxdynoEc/800x799',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'https://source.unsplash.com/qDkso9nvCg0/600x799',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://source.unsplash.com/iecJiKe_RNg/600x799',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://source.unsplash.com/epcsn8Ed8kY/600x799',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://source.unsplash.com/NQSWvyVRIJk/800x599',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://source.unsplash.com/zh7GEuORbUw/600x799',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://source.unsplash.com/PpOHJezOalU/800x599',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://source.unsplash.com/I1ASdgphUH4/800x599',
+    width: 4,
+    height: 3
+  }
+]
 
 export const kitchenphotos = [
-    {
-      src: Kitchen1,
-      width: 4,
-      height: 3,
-    },
-    {
-      src: Kitchen2,
-      width: 1,
-      height: 1
-    },
-    {
-      src: Kitchen3,
-      width: 3,
-      height: 4
-    },
-    {
-      src: Kitchen4,
-      width: 3,
-      height: 4
-    },
-    {
-      src: Kitchen5,
-      width: 3,
-      height: 4
-    },
-    {
-      src: Kitchen6,
-      width: 4,
-      height: 3
-    },
-    {
-      src: Kitchen7,
-      width: 3,
-      height: 4
-    },
-    {
-      src: Kitchen8,
-      width: 4,
-      height: 3
-    },
-    {
-      src: Kitchen9,
-      width: 4,
-      height: 3
-    }
-  ];
-  
+  {
+    src: Kitchen1,
+    width: 4,
+    height: 3
+  },
+  {
+    src: Kitchen2,
+    width: 1,
+    height: 1
+  },
+  {
+    src: Kitchen3,
+    width: 3,
+    height: 4
+  },
+  {
+    src: Kitchen4,
+    width: 3,
+    height: 4
+  },
+  {
+    src: Kitchen5,
+    width: 3,
+    height: 4
+  },
+  {
+    src: Kitchen6,
+    width: 4,
+    height: 3
+  },
+  {
+    src: Kitchen7,
+    width: 3,
+    height: 4
+  },
+  {
+    src: Kitchen8,
+    width: 4,
+    height: 3
+  },
+  {
+    src: Kitchen9,
+    width: 4,
+    height: 3
+  }
+]
+
 export const bathroomphotos = [
-    {
-      src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
-      width: 4,
-      height: 3
-    },
-    {
-      src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
-      width: 1,
-      height: 1
-    },
-    {
-      src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
-      width: 3,
-      height: 4
-    },
-    {
-      src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
-      width: 3,
-      height: 4
-    },
-    {
-      src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
-      width: 3,
-      height: 4
-    },
-    {
-      src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
-      width: 4,
-      height: 3
-    },
-    {
-      src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
-      width: 3,
-      height: 4
-    },
-    {
-      src: "https://source.unsplash.com/PpOHJezOalU/800x599",
-      width: 4,
-      height: 3
-    },
-    {
-      src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
-      width: 4,
-      height: 3
-    }
-  ];
-  
+  {
+    src: 'https://source.unsplash.com/2ShvY8Lf6l0/800x599',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://source.unsplash.com/Dm-qxdynoEc/800x799',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'https://source.unsplash.com/qDkso9nvCg0/600x799',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://source.unsplash.com/iecJiKe_RNg/600x799',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://source.unsplash.com/epcsn8Ed8kY/600x799',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://source.unsplash.com/NQSWvyVRIJk/800x599',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://source.unsplash.com/zh7GEuORbUw/600x799',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://source.unsplash.com/PpOHJezOalU/800x599',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://source.unsplash.com/I1ASdgphUH4/800x599',
+    width: 4,
+    height: 3
+  }
+]
+
 export const otherphotos = [
-    {
-      src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
-      width: 4,
-      height: 3
-    },
-    {
-      src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
-      width: 1,
-      height: 1
-    },
-    {
-      src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
-      width: 3,
-      height: 4
-    },
-    {
-      src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
-      width: 3,
-      height: 4
-    },
-    {
-      src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
-      width: 3,
-      height: 4
-    },
-    {
-      src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
-      width: 4,
-      height: 3
-    },
-    {
-      src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
-      width: 3,
-      height: 4
-    },
-    {
-      src: "https://source.unsplash.com/PpOHJezOalU/800x599",
-      width: 4,
-      height: 3
-    },
-    {
-      src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
-      width: 4,
-      height: 3
-    }
-  ];
-  
+  {
+    src: 'https://source.unsplash.com/2ShvY8Lf6l0/800x599',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://source.unsplash.com/Dm-qxdynoEc/800x799',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'https://source.unsplash.com/qDkso9nvCg0/600x799',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://source.unsplash.com/iecJiKe_RNg/600x799',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://source.unsplash.com/epcsn8Ed8kY/600x799',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://source.unsplash.com/NQSWvyVRIJk/800x599',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://source.unsplash.com/zh7GEuORbUw/600x799',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://source.unsplash.com/PpOHJezOalU/800x599',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://source.unsplash.com/I1ASdgphUH4/800x599',
+    width: 4,
+    height: 3
+  }
+]
